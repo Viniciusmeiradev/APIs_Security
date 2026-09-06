86ac569654fb38a97e2f26cc67a40d54219d95ef
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-06 13:33:15 -0300

    Refactor 2FA authentication logic and variable names
    Updated variable names for consistency and added console logs for debugging.

--- autenticacao_2fa/controller/authcontroller.js
+++ autenticacao_2fa/controller/authcontroller.js
@@ -13,31 +13,31 @@ exports.login = (req, res) =>{
     }
     //2FA ativado, token disponivel por 2 minutos   
     if(usuario.is_2fa_enabled){
-        const tempToken = jwt.sign({ usuarioId: usuario.id, type: '2fa_peding'}, JWT_SECRET, {expiresIn: '2m'});
+        const tempToken = jwt.sign({ usuarioId: usuario.id, type: '2fa_pending'}, JWT_SECRET, {expiresIn: '2m'});
         return res.json({
-            require_2fa: true,
-            temp_Token: tempToken
+            requires_2fa: true,
+            temp_token: tempToken
         });
     }
 
     //2FA desativado, token fixo 
     const authToken = jwt.sign({usuarioId: usuario.id}, JWT_SECRET, {expiresIn: '1h'});
     return res.json({
-        require_2fa: false,
+        requires_2fa: false,
         token: authToken
     });
 };
 
 //Validacao no 2FA aplicativo
 exports.verify2FA = (req, res) =>{
-    const {temp_Token, token_2fa} = req.body;
-    if(!temp_Token || !token_2fa){
+    const {temp_token, token_2fa} = req.body;
+    if(!temp_token || !token_2fa){
         return res.status(400).json({error: 'Parâmetros insuficientes!'});
     } 
 
     try{
         //Decodifica e valida o token temporario
-        const decode = jwt.verify(temp_Token, JWT_SECRET);
+        const decode = jwt.verify(temp_token, JWT_SECRET);
         if(decode.type !== '2fa_pending'){
             return res.status(401).json({error: 'Token inválido!'});
         }
@@ -46,6 +46,15 @@ exports.verify2FA = (req, res) =>{
         if(!usuario){
             return res.status(404).json({error: 'Usuario não encontrado!'});
         }
+
+        //confere no terminal se o codigo bate
+        const codigoServidor = speakeasy.totp({
+            secret: usuario.secret_key,
+            encoding: 'base32'
+        });
+        console.log('Código digitado:', token_2fa);
+        console.log('Código esperado pelo servidor:', codigoServidor);
+
 
         //Valida o código de 6 digitos
         const verifica = speakeasy.totp.verify({
