6853056eef0b3410f1b82b2d73e53873b0b24550
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-12 14:59:11 -0300

    Update User.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/entities/User.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/entities/User.java
@@ -9,9 +9,50 @@ public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
     private UUID userId;
 
+    @Column(unique = true)
     private String nome;
+
     private String senha;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "tb_usuarios_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles;
+
+
+    public UUID getUserId(){
+        return userId;
+    }
+    public void setUserId(UUID userId){
+        this.userId = userId;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    public Set<Role> getRoles(){
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
 }
