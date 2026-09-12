a04e73cf3431259df45a778895c4e7243281439c
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-12 14:58:31 -0300

    Update Tweet.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/entities/Tweet.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/entities/Tweet.java
@@ -8,11 +8,41 @@ public class Tweet{
     
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
     private Long tweetId;
 
     private User usuario;
     private String conteudo;
 
     @CreationTimestamp
-    private Instant CreationTimestamp;
+    private Instant creationTimestamp;
+
+    public Long getTweetId(){
+        return tweetId;
+    }
+    public void setTweetId(Long tweetId){
+        this.tweetId = tweetId;
+    }
+
+    public User getUsuario(){
+        return usuario;
+    }
+    public void setUsuario(User usuario){
+        this.usuario = usuario;
+    }
+
+    public String getConteudo(){
+        return conteudo;
+    }
+    public void setConteudo(String conteudo){
+        this.conteudo = conteudo;
+    }
+
+    public Instant getCreationTimestamp(){
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp){
+        this.creationTimestamp = creationTimestamp;
+    }
 }
