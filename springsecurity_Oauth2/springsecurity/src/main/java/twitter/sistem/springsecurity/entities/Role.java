1e3306dc0284944363d27f8a4bf97692c90299d6
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-12 14:57:33 -0300

    Update Role.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/entities/Role.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/entities/Role.java
@@ -8,7 +8,35 @@ public class Role{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long roleId;
 
     private String rolenome;
+
+    public Long getRoleId(){
+        return roleId;
+    }
+    public void setRoleId(Long roleId){
+        this.roleId = roleId;
+    }
+
+    public String getRolenome(){
+        return rolenome;
+    }
+    public void setRolenome(String rolenome){
+        this.rolenome = rolenome;
+    }
+
+    public enum Values{
+        ADMIN(1L),
+        BASIC(2L);
+        long roleId;
+
+        Values(Long roleId){
+            this.roleId = roleId;
+        }
+        public Long getRoleId(){
+            return roleId;
+        }
+    }
 }
