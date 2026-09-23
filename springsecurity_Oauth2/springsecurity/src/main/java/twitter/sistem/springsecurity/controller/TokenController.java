86b79b6023d6a986d3757f9d79306ccdb75e6b4c
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-22 22:38:39 -0300

    Update TokenController.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/controller/TokenController.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/controller/TokenController.java
@@ -2,16 +2,25 @@ package twitter.sistem.springsecurity.controller;
 
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import twitter.sistem.springsecurity.controller.dto.LoginRequest;
+import twitter.sistem.springsecurity.controller.dto.LoginResponse;
+import twitter.sistem.springsecurity.repository.UserRepository;
 
 @RestController
 public class TokenController {
     private final JwtEncoder jwtEnconder;
+    private final UserRepository userRepository;
 
     public TokenController(JwtEncoder jwtEncoder){
         this.jwtEncoder = jwtEncoder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<>
+    public ResponseEntity<LoginResponse>login(@RequestBody LoginRequest loginRequest){
+        userRepository.findBynome(loginRequest.nome());
+    }
 }
+
