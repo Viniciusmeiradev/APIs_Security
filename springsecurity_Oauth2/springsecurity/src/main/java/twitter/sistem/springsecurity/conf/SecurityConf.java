68953063486ac3a7ccd803b4abb04bc3c31b4485
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-23 18:45:27 -0300

    Add PasswordEncoder bean to SecurityConf

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/conf/SecurityConf.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/conf/SecurityConf.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -46,5 +47,10 @@ public class SecurityConf{
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
         var jwks = new immutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean 
+    public PasswordEncoder passwordEncoder(){
+        return new PasswordEncoder();
     }
 }
