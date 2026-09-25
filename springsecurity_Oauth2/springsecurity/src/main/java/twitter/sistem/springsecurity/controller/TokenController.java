fbc4cb68b9d0b63cf8449f0127795068cd8f45d9
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-24 22:52:54 -0300

    Update TokenController.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/controller/TokenController.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/controller/TokenController.java
@@ -1,6 +1,9 @@
 package twitter.sistem.springsecurity.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,18 +17,24 @@ import twitter.sistem.springsecurity.repository.UserRepository;
 public class TokenController {
     private final JwtEncoder jwtEncoder;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public TokenController(JwtEncoder jwtEncoder, UserRepository userRepository){
+    public TokenController(JwtEncoder jwtEncoder, UserRepository userRepository, PasswordEncoder passwordEncoder;){
         this.jwtEncoder = jwtEncoder;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse>login(@RequestBody LoginRequest loginRequest){
         var user = userRepository.findBynome(loginRequest.nome());
 
-        if(user.isEmpty()){
+        if(user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)){
             throw new BadCredentialsException("Usuario ou senha inválido!");
         }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+        var claims = JwtClaimsSet.builder().issuer("backend").subject(user.get().getUserId().toString()).issuedAt(now).expiresAt(now.plusSeconds(expiresIn));
     }
 }
