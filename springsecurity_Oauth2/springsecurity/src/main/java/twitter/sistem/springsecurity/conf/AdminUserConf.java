11d90bf927717d56a512fddb9368eef66765af80
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-25 23:48:16 -0300

    Update AdminUserConf.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/conf/AdminUserConf.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/conf/AdminUserConf.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import jakarta.transaction.Transactional;
+import twitter.sistem.springsecurity.entities.Role;
 import twitter.sistem.springsecurity.repository.RoleRepository;
 import twitter.sistem.springsecurity.repository.UserRepository;
 
@@ -23,7 +24,20 @@ public class AdminUserConf implements CommandLineRunner {
     @Override
     @Transactional 
     public void run(String args) throws Exception{
-        var roleAdmin = roleRepository.findByName()
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+        var userAdmin = userRepository.findByUsername("admin");
         
+        userAdmin.ifPresentOrElse{
+            user -> {
+                System.out.println("Admin já existe")
+            };
+            () -> {
+                var user = new User();
+                user.setUsername("Admin");
+                user.setPassword(passwordEncoder.encode(rawPassword:"123"));
+                user.setRoles(Set.of(roleAdmin));
+            }
+        };
     }
 }
+
