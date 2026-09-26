255e458018e2d4d91ac1eca354f6619a23d9869f
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-09-25 23:05:05 -0300

    Update AdminUserConf.java

--- springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/conf/AdminUserConf.java
+++ springsecurity_Oauth2/springsecurity/src/main/java/twitter/sistem/springsecurity/conf/AdminUserConf.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import jakarta.transaction.Transactional;
 import twitter.sistem.springsecurity.repository.RoleRepository;
 import twitter.sistem.springsecurity.repository.UserRepository;
 
@@ -13,12 +14,16 @@ public class AdminUserConf implements CommandLineRunner {
     private UserRepository userRepository;
     private BCryptPasswordEncoder passwordEncoder;
 
-    public AdminUserConf(BCryptPasswordEncoder passwordEncoder){
+    public AdminUserConf(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    @Override 
+    @Override
+    @Transactional 
     public void run(String args) throws Exception{
+        var roleAdmin = roleRepository.findByName()
         
     }
 }
